56edaecfcda07b907c63788054d4a7a54b05a725
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-03-28 22:24:18 +0300

    create navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<header>
+    <nav>
+        <ul class="navbar_links">
+          <li><a href="{base}/">Home</a></li>
+          <li><a href="{base}/">About</a></li>
+          <li><a href="{base}/">Create Account</a></li>
+          <li><a href="{base}/">Sign In</a></li>
+        </ul>
+    </nav>
+</header>
