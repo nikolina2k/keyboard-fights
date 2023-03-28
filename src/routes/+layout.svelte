9002ec37dc3ba786d152970154b6b0d729917eb2
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-03-28 23:19:26 +0300

    style stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,19 @@
 	import Navbar from '../components/Navbar.svelte';
 </script>
 
+<div class="container">
 <Navbar />
 <slot />
+</div>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+    </style>
