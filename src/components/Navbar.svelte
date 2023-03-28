4f92e780dd613dcfd9a163e8d217a6f6a1bbe477
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-03-28 23:27:57 +0300

    add about path to navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -6,7 +6,7 @@
     <nav>
         <ul class="navbar_links">
           <li><a href="{base}/">Home</a></li>
-          <li><a href="{base}/">About</a></li>
+          <li><a href="{base}/about/">About</a></li>
           <li><a href="{base}/">Create Account</a></li>
           <li><a href="{base}/">Sign In</a></li>
         </ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import Home from '../components/Home.svelte';
+    import About from '../components/About.svelte';
+import Home from '../components/Home.svelte';
 import Navbar from '../components/Navbar.svelte';
 </script>
 
