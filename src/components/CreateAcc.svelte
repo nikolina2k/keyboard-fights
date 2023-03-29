1d3960a85bbdec63bae62b5369e82ab41b215df1
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-03-29 23:47:25 +0300

    add a form to create account page

--- src/components/CreateAcc.svelte
+++ src/components/CreateAcc.svelte
@@ -2,7 +2,35 @@
 </script>
 
 <div class="create-acc">
-    <h1>Create Account</h1>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+        <div class="card">
+          <h2 class="card-title text-center">Create Account</h2>
+            <div class="card-body py-md-4">
+              <form _lpchecked="1">
+              <div class="form-group">
+                <input type="text" class="form-control" id="name" placeholder="Name">
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" id="email" placeholder="Email">
+              </div>                     
+              <div class="form-group">
+                <input type="password" class="form-control" id="password" placeholder="Password">
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" id="confirm-password" placeholder="Confirm password">
+              </div>
+              <div class="d-flex flex-row align-items-center justify-content-between">
+                <a href="#">Login</a>
+                <button class="btn btn-primary">Create Account</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
