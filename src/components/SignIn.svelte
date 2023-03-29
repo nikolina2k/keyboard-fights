5d9539552f67b915a545293128c384953bdb0d98
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-03-29 23:32:53 +0300

    style sign in page

--- src/components/SignIn.svelte
+++ src/components/SignIn.svelte
@@ -10,6 +10,21 @@
   .sign-in {
     display: table;
     margin: auto;
+    background: rgba(0, 0, 0, .4);
+    border-radius: 100px;
+    box-shadow: #f05e5e 0 10px 20px -10px;
+    box-sizing: border-box;
+    color:white;
+    font-family: CerebriSans-Regular,-apple-system,system-ui,Roboto,sans-serif;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 24px;
+    opacity: 1;
+    outline: 0 solid transparent;
+    padding: 8px 18px;
+    width: fit-content;
+    word-break: break-word;
+    border: 0;
   }
 
 </style>
