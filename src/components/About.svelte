27542f5b1f3cb283a1076b8bfcd3987fcdbdf310
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-06 23:22:54 +0300

    change style

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -9,23 +9,31 @@
 <style>
 
   .about {
-    display: table;
-    margin: auto;
-    background: rgba(0, 0, 0, .4);
-    border-radius: 100px;
+    margin: 20px auto;
+    width: 343px; 
+    height: 464px;
+    border-radius: 10px;
     box-shadow: #f05e5e 0 10px 20px -10px;
-    box-sizing: border-box;
-    color:white;
-    font-family: CerebriSans-Regular,-apple-system,system-ui,Roboto,sans-serif;
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 24px;
-    opacity: 1;
-    outline: 0 solid transparent;
-    padding: 8px 18px;
-    width: fit-content;
-    word-break: break-word;
-    border: 0;
+    border: none;
+    background-color: rgba(0, 0, 0, .4);
+    backdrop-filter: blur(10px);display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
   }
+
+h1 {
+  font-size: 32px;
+  font-weight: 300;
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-weight: 300;
+  color: white;
+  text-align: center;
+  margin: 70px;
+}
 
 </style>
