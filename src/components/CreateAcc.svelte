f6e48fb2cb6f86481906fb278f6cf840e568ebba
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-06 22:48:33 +0300

    style form

--- src/components/CreateAcc.svelte
+++ src/components/CreateAcc.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
+  import { base } from '$app/paths';
 </script>
 
 <div class="card">
   <h1>Create an account</h1>
 
   <form action="/">
-  <label for="name"><i class="icon-envelope "></i></label>
   <input type="text" name="name" id="name" placeholder="Email" required/>
-  <label for="name"><i class="icon-user"></i></label>
   <input type="text" name="name" id="name" placeholder="Name" required/>
-  <label for="name"><i class="icon-shield"></i></label>
   <input type="password" name="name" id="name" placeholder="Password" required/>
-  <label for="name"><i class="icon-shield"></i></label>
   <input type="password" name="name" id="name" placeholder="Confirm password" required/>
-  <a href="#" class="button">Create</a>
+  <a href="{base}" class="button">Create</a>
   </form>
 </div>
 
@@ -28,7 +25,7 @@ div, h1, form, input {
 h1 {
   font-size: 32px;
   font-weight: 300;
-  color: black;
+  color: white;
   text-align: center;
   padding-top: 10px;
   margin-bottom: 10px;
@@ -37,15 +34,13 @@ h1 {
 .card {
   margin: 20px auto;
   width: 343px; 
-  height: 464px; 
-  -webkit-border-radius: 8px/7px; 
-  -moz-border-radius: 8px/7px; 
+  height: 464px;
   border-radius: 8px/7px; 
-  background-color: #ebebeb; 
-  -webkit-box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
-  -moz-box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
-  box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
-  border: solid 1px #cbc9c9;
+  background-color: #ebebeb;
+  box-shadow: 1px 2px 5px rgba(0,0,0,.31);
+  border: none;
+  background-color: rgba(0, 0, 0, .4);
+  backdrop-filter: blur(10px);
 }
 
 form{
@@ -54,13 +49,9 @@ form{
 
 input[type=text],input[type=password]{
   width: 200px; 
-  height: 39px; 
-  -webkit-border-radius: 0px 4px 4px 0px/5px 5px 4px 4px; 
-  -moz-border-radius: 0px 4px 4px 0px/0px 0px 4px 4px; 
+  height: 39px;
   border-radius: 0px 4px 4px 0px/5px 5px 4px 4px; 
-  background-color: #fff; 
-  -webkit-box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
-  -moz-box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
+  background-color: #fff;
   box-shadow: 1px 2px 5px rgba(0,0,0,.09); 
   border: solid 1px #cbc9c9;
   margin-left: -5px;
@@ -87,13 +78,11 @@ a.button {
 
 a.button:hover {
   top: 3px;
-  background-color: grey;
+  background-color: green;
   -webkit-box-shadow: none; 
   -moz-box-shadow: none; 
   box-shadow: none;
   
 }
-
-
 
 </style>
