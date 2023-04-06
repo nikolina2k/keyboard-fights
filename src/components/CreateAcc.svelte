b0a56547407a2da544a109951f025dbd625e6520
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-06 22:26:13 +0300

    make a new form for creating an account

--- src/components/CreateAcc.svelte
+++ src/components/CreateAcc.svelte
@@ -1,96 +1,24 @@
 <script lang="ts">
 </script>
 
-<div class="create-acc">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-5">
-        <div class="card">
-          <h2 class="card-title text-center">Create Account</h2>
-            <div class="card-body py-md-4">
-              <form _lpchecked="1">
-              <div class="form-group">
-                <input type="text" class="form-control" id="name" placeholder="Name">
-              </div>
-              <div class="form-group">
-                <input type="email" class="form-control" id="email" placeholder="Email">
-              </div>                     
-              <div class="form-group">
-                <input type="password" class="form-control" id="password" placeholder="Password">
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control" id="confirm-password" placeholder="Confirm password">
-              </div>
-              <div class="d-flex flex-row align-items-center justify-content-between">
-                <button class="btn btn-primary">Create Account</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="testbox">
+  <h1>Create an account</h1>
+
+  <form action="/">
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="Email" required/>
+  <label id="icon" for="name"><i class="icon-user"></i></label>
+  <input type="text" name="name" id="name" placeholder="Name" required/>
+  <label id="icon" for="name"><i class="icon-shield"></i></label>
+  <input type="password" name="name" id="name" placeholder="Password" required/>
+  <label id="icon" for="name"><i class="icon-shield"></i></label>
+  <input type="password" name="name" id="name" placeholder="Confirm password" required/>
+  <a href="#" class="button">Create</a>
+  </form>
 </div>
 
 <style>
 
-  .create-acc {
-    display: table;
-    margin: auto;
-    background: rgba(0, 0, 0, .4);
-    border-radius: 100px;
-    box-shadow: #f05e5e 0 10px 20px -10px;
-    box-sizing: border-box;
-    color:white;
-    font-family: CerebriSans-Regular,-apple-system,system-ui,Roboto,sans-serif;
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 24px;
-    opacity: 1;
-    outline: 0 solid transparent;
-    padding: 8px 18px;
-    width: fit-content;
-    word-break: break-word;
-    border: 0;
-  }
 
-  h2{
-    padding-top: 1.5rem;
-  }
-  
-  .card{
-    border: 0.40rem solid #f8f9fa;
-    top: 10%;
-  }
-  .form-control{
-    background-color: #f8f9fa;
-    padding: 20px;
-    padding: 25px 15px;
-    margin-bottom: 1.3rem;
-  }
-
-  .form-control:focus {
-
-      color: #000000;
-      background-color: #ffffff;
-      border: 3px solid #da5767;
-      outline: 0;
-      box-shadow: none;
-
-  }
-
-  .btn{
-    padding: 0.6rem 1.2rem;
-    background: #da5767;
-    border: 2px solid #da5767;
-  }
-  .btn-primary:hover {
-
-      
-      background-color: #df8c96;
-      border-color: #df8c96;
-    transition: .3s;
-
-  }
 
 </style>
