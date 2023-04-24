91b8308b077d953cd2bf631e93d67a5c4e09cdec
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-24 23:30:35 +0300

    style the select race page

--- src/lib/components/SelectRace.svelte
+++ src/lib/components/SelectRace.svelte
@@ -5,6 +5,7 @@
       goto('../race');
     }
 </script>
+
 <div class="selectRace">
     <h1>Select race </h1>
     Please pick the dictionary of words you'd like to race with:
@@ -12,15 +13,54 @@
       <button on:click={startRaceClick} type="button" class="join">Random words</button>
     </li>
 </div>
+
 <style>
   .selectRace{
     color: white;
-          font-family: sans-serif;
-      font-weight: bold;
-          padding: 12px 16px;
-      margin: 0 8px;
-          position: relative;
-      cursor: pointer;
-          white-space: nowrap;
+    font-family: sans-serif;
+    padding: 2rem;
+    margin: 20px auto;
+    width: auto; 
+    height: 464px;
+    border-radius: 10px;
+    box-shadow: #f05e5e 0 10px 20px -10px;
+    border: none;
+    background-color: rgba(0, 0, 0, .4);
+    backdrop-filter: blur(10px);display: flex;
+    flex-direction: column;
+    justify-content: top;
+    text-align: left;
+    
+    white-space: nowrap;
   }
+
+  button {
+    cursor: pointer;
+    font-size: 14px;
+  font-weight: 600;
+  padding: 7px 20px;
+  margin-top: 30px;
+  margin-left: auto;
+  display: inline-block;
+  text-decoration: none;
+  width: 50%;
+  border-radius: 100px;
+  transition: all 250ms;
+  border: 0;
+  font-size: 16px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  background-color: white;
+  box-shadow: rgba(44, 187, 99, .2) 0 -25px 18px -14px inset,rgba(44, 187, 99, .15) 0 1px 2px,rgba(44, 187, 99, .15) 0 2px 4px,rgba(44, 187, 99, .15) 0 4px 8px,rgba(44, 187, 99, .15) 0 8px 16px,rgba(44, 187, 99, .15) 0 16px 32px;
+  color: black;
+  }
+
+  button:hover {
+  top: 3px;
+  cursor: pointer;
+  box-shadow: rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px;
+  transform: scale(1.05);
+  }
+
 </style>
