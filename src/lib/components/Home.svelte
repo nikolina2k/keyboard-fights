5ec27287a34a4d145bc3a44033c3706aa751802b
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-24 20:45:15 +0300

    fix home page styling

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -10,26 +10,20 @@
     }
 </script>
 
-<div class="home">
-  <div class="buttons">
-    <button on:click={joinRaceClick} type="button" class="join">Join a typing race</button>
-    <button on:click={createRaceClick} type="button" class="create">Create a typing race</button>
-  </div>
+<div class="buttons">
+  <button on:click={joinRaceClick} type="button" class="join">Join a typing race</button>
+  <button on:click={createRaceClick} type="button" class="create">Create a typing race</button>
 </div>
 
 <style>
 
-  .home {
-    display: table;
-    margin: auto;
-  }
-  
   .buttons {
-    margin: auto;
+    display: table;
+    margin: 20px auto;
     gap: 2rem;
-    width: 303px; 
+    width: 343px; 
     height: 464px;
-    border-radius: 15px;
+    border-radius: 10px;
     box-shadow: #f05e5e 0 10px 20px -10px;
     border: none;
     background-color: rgba(0, 0, 0, .4);
@@ -63,17 +57,6 @@
 
   .join:hover {
     box-shadow: rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px;
-    transform: scale(1.05);
-  }
-
-  .practice {
-    background-color: #dd3489;
-    box-shadow: rgba(185, 187, 44, 0.2) 0 -25px 18px -14px inset,rgba(165, 187, 44, 0.15) 0 1px 2px,rgba(177, 187, 44, 0.15) 0 2px 4px,rgba(177, 187, 44, 0.15) 0 4px 8px,rgba(185, 187, 44, 0.15) 0 8px 16px,rgba(173, 187, 44, 0.15) 0 16px 32px;
-    color: white;
-  }
-
-  .practice:hover {
-    box-shadow: rgba(173, 187, 44, 0.35) 0 -25px 18px -14px inset,rgba(177, 187, 44, 0.25) 0 1px 2px,rgba(173, 187, 44, 0.25) 0 2px 4px,rgba(165, 187, 44, 0.25) 0 4px 8px,rgba(161, 187, 44, 0.25) 0 8px 16px,rgba(161, 187, 44, 0.25) 0 16px 32px;
     transform: scale(1.05);
   }
 
