228a654f5f945f8676586685574e500169c24bb6
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-25 13:00:59 +0300

    style the user profile page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -18,7 +18,18 @@
 </div>
 
 <style>
-div {
+.card {
+    margin: 20px auto;
+    width: 343px; 
+    height: 464px;
+    border-radius: 10px;
+    box-shadow: #f05e5e 0 10px 20px -10px;
+    border: none;
+    background-color: rgba(0, 0, 0, .4);
+    backdrop-filter: blur(10px);display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
     color: white;
 }
 </style>
